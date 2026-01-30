bbd9795f9d7d1d69b879d10bac63283e41ab327a
Sanjai <167332993+Sanjai-J-Official@users.noreply.github.com>
2026-01-30 23:00:27 +0530

    Add count component with increment and decrement buttons

---
+++ Mini  Project/count.jsx
@@ -0,0 +1,21 @@
+ import {useState,useEffect} from "react"
+
+function App(){
+
+ const [count,setCount]=useState(0);
+
+ useEffect(()=>{
+
+  console.log("Count Changed:",count);
+  
+ },[count])
+  return (<>
+   
+  <h1>Count:{count} </h1>
+  <button class="button" onClick={()=>setCount(count+1)}>increase</button>
+<button class="button" onClick={()=>setCount(count-1)}>Decrease</button>
+  
+  </>)
+}
+
+ export default App
