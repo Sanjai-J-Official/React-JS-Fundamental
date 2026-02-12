6896bc6074fedbf2d9ca19031312dde04dfe8c60
Sanjai <167332993+Sanjai-J-Official@users.noreply.github.com>
2026-02-13 00:30:54 +0530

    Update formSubmit.jsx

--- Mini  Task/formSubmit.jsx
+++ Mini  Task/formSubmit.jsx
@@ -1,34 +1,98 @@
  import {useState,useEffect} from "react"
-import Product from "./product"
+ 
 function App(){
 
- const [val, setVal]=useState("")
-  
+ const [form,setForm]=useState({
+    name:"",
+    email:"",
+    password:"",
+    agree:false
+    })
+  const [ Error, setError] = useState("")
+  const [ success, setSuccess] = useState("")
+
+ 
 function handleSubmit(e){
 
     e.preventDefault();
-    console.log(val);
-    
+ if (!form.name || !form.email || !form.password || !form.agree){
 
+    setError("Fill Missing Details")
+setSuccess("");
+return
+
+ }
+ else{
+
+     setError("");
+     setSuccess("Successfully Register..!");
+
+ }
 }
 
-    return(
-       <>
-       
-       <form onSubmit={handleSubmit} >
-            <h1> Form Handling </h1>
-            <input 
-            type="name"
-            placeholder="enter"
-            onChange={(e)=> setVal(e.target.value)}
-            />
-            <button type="submit"> Submit</button>
-                </form>
-                </>
-            )
-    
+function handleForm(e){
+    setForm({
+        ...form,
+        [e.target.name]:e.target.value
+    })
+}
+
+    return( 
+            
+    <>
+    <h1>
+        Form Register
+    </h1>
+    <form onSubmit={handleSubmit}>
+<input 
+name="name" 
+type="text" 
+value={form.name}
+placeholder="Enter the Name" 
+onChange={handleForm}
+
+/>
 
 
+<input 
+name="email" 
+value={form.email}
+type="email" 
+placeholder="Enter the Email" 
+onChange={handleForm}
+/>
+
+
+<input 
+name="password" 
+value={form.password}
+type="password" 
+placeholder="Enter the Password" 
+onChange={handleForm}
+/>
+<br />
+<br />
+<label>
+<input 
+name="agree" 
+value={form.agree}
+type="checkbox" 
+onChange={handleForm} 
+/>
+ 
+I agree to   <a href="" style={{color:"red"}}>terms</a>  & Condition
+</label>
+<button>Submit</button>
+
+    </form>
+
+    {Error && <p style={{color:"red"}}> {Error} </p>} 
+    {success && <p style={{color:"Green"}}> {success} </p>}
+    </>
+
+    )
 }
  
  export default App
+
+ 
