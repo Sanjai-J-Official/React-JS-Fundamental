d49b20492ed7ad086cd94361ee06ac7a280ce6cb
Sanjai <167332993+Sanjai-J-Official@users.noreply.github.com>
2026-02-21 23:54:59 +0530

    Create useEffect-SetInterval.jsx

---
+++ Mini  Task/useEffect-SetInterval.jsx
@@ -0,0 +1,39 @@
+ import {useState,useEffect} from "react"
+ 
+function App(){
+ 
+ const [count, setCount]= useState(0);
+
+ useEffect(()=>{
+ 
+const timer =setInterval(()=>{
+setCount(prev=>prev+1)
+},1000);
+
+if (count===5){
+  clearInterval(timer)
+}
+
+return ()=>{
+  clearInterval(timer)
+}
+ },[count])
+ return (
+< >
+ 
+ <div style={{textAlign:"center"}}>
+  <h1 >Timer Count</h1>
+  <h2>
+    {count}
+    
+  </h2>
+{count >=5 && <p>Finished</p> }
+
+ </div>
+</>
+    )
+}
+ 
+ export default App
+
+ 
