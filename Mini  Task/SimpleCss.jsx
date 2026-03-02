3ce265ac57557e1f5c4897bd2cea89e41be60a8d
Sanjai <167332993+Sanjai-J-Official@users.noreply.github.com>
2026-03-03 00:54:08 +0530

    Create SimpleCss.jsx

---
+++ Mini  Task/SimpleCss.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import  "./App.css";
+function App() {
+
+ 
+
+  return (
+
+    <div style={{ textAlign: "center" }}>
+   
+<h1>This is Css Part</h1>
+
+<div className="Card">
+  This is Content Area
+</div>
+
+    </div>
+  );
+}
+
+export default App;
+.Card {
+  
+    width:Auto;
+    border: 5px solid black;
+    margin: 99px;
+    color: red;
+}
